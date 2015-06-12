b5ef7e8edad193583247a8f4e2125439a74a975e
chikeichan <chikeichan@gmail.com>
2015-06-11 23:17:56 -0700

    Added pairSum

---
+++ src/pairSum.js
@@ -0,0 +1,20 @@
+/**
+ * pairSum should take in an array of integers, and a sum.
+ * It should return a boolean value representing whether or not 
+ * any two integers adds up to the sum.
+ *
+ * For Example:
+ *
+ * pairSum([1,3,5,7], 9)   ===> false;
+ * pairSum([1,3,5,7], 8)   ===> true;
+ * pairSum([1,3,5,7], 14)  ===> false;
+ * pairSum([0, 0, 1], 0)   ===> true;
+ * 
+ * What is the Time Complexity of your solution?
+ * Can you do it in linear time?
+ * 
+ */
+
+function pairSum(array, sum) {
+  // Start Here
+};
